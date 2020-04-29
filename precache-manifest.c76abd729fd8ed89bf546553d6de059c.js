d4736720180298442533f5c981614a25175b6345
Keeth <keerthi.mac@gmail.com>
2020-04-29 17:24:15 +0530

    Updates

---
+++ precache-manifest.c76abd729fd8ed89bf546553d6de059c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0e60e992b0b5c597b97cfc43ad60ac",
+    "url": "/React-app-1/index.html"
+  },
+  {
+    "revision": "87ed5c3518a4a68fc401",
+    "url": "/React-app-1/static/css/main.05f0aa38.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/React-app-1/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-1/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ed5c3518a4a68fc401",
+    "url": "/React-app-1/static/js/main.bcf6b5a1.chunk.js"
+  },
+  {
+    "revision": "75fa99fb2ec866da69de",
+    "url": "/React-app-1/static/js/runtime-main.edd2378c.js"
+  }
+]);
